a039b58b4a052a5b4122a06b605d8b4d3c2ef128
Sebastian Kloppe <yeahdev@gmail.com>
2025-06-22 20:54:12 +0200

    init flowbite

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected title = 'yeah-med-fe';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
