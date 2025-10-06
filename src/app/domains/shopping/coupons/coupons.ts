6e3b6ee2dd754b7121d9168019370e9b0a6d856e
Sebastian Kloppe <yeahdev@gmail.com>
2025-10-06 14:26:16 +0200

    #13: add translations, move coupons to own component

---
+++ src/app/domains/shopping/coupons/coupons.ts
@@ -0,0 +1,147 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { DatePipe } from '@angular/common';
+
+import {
+  MatBottomSheet,
+  MatBottomSheetModule,
+} from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { TranslocoDirective } from '@jsverse/transloco';
+
+import { HttpService } from '../../../core/services/http-service';
+import { IsExpiredPipe } from '../../dashboard/pipes/table-pipes';
+import { CouponResponse, CouponRequest } from '../models';
+import { CouponSheet } from '../sheets/coupon-sheet';
+
+@Component({
+  selector: 'ym-shopping-coupons',
+  template: `
+    <ng-container *transloco="let t; prefix: 'shopping.coupons'">
+      <div class="title">
+        <span>{{ t('title') }}</span>
+      </div>
+
+      <div class="list">
+        @if (couponRef.isLoading()) {
+        <h2>{{ t('loading') }}</h2>
+        } @else if (couponRef.error()) {
+        <h2>{{ t('error') }}</h2>
+        <div>{{ couponRef.error()?.message }}</div>
+        } @else {
+        <!-- -->
+        <mat-list>
+          @for (coupon of couponRef.value(); track coupon.id) {
+          <mat-list-item [class.expired]="coupon.expiredAt | isExpired">
+            <mat-icon matListItemIcon svgIcon="coupon">coupon</mat-icon>
+            <div matListItemMeta>
+              <mat-icon (click)="handleEdit(coupon)">edit</mat-icon>
+              <mat-icon (click)="handleDelete(coupon)">delete</mat-icon>
+            </div>
+            <div matListItemTitle>{{ coupon.name }}</div>
+            <div matListItemLine>{{ coupon.expiredAt | date }}</div>
+            @if(coupon.website) {
+            <div matListItemLine>
+              <a [href]="coupon.website" target="_blank" rel="noreferrer">{{
+                coupon.website
+              }}</a>
+            </div>
+            }
+          </mat-list-item>
+          @if (!$last) {
+          <mat-divider></mat-divider>
+          }
+          <!-- -->
+          } @empty {
+          <h2>{{ t('no-data') }}</h2>
+          }
+          <!-- -->
+        </mat-list>
+        }
+      </div>
+
+      <div class="actions">
+        <button matFab (click)="openCouptonSheet()">
+          <mat-icon>add</mat-icon>
+        </button>
+      </div>
+    </ng-container>
+  `,
+  styleUrl: './coupons.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    DatePipe,
+    IsExpiredPipe,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    TranslocoDirective,
+  ],
+})
+export class CouponsComponent {
+  private readonly bottomSheet = inject(MatBottomSheet);
+  private readonly httpService = inject(HttpService);
+
+  protected couponRef = rxResource({
+    stream: () => this.httpService.get<CouponResponse[]>('/coupons'),
+    defaultValue: [],
+  });
+
+  handleDelete(coupon: CouponResponse) {
+    const confirmed = confirm(
+      'Are you sure to delete the coupon: ' + coupon.name + '?'
+    );
+
+    if (confirmed) {
+      this.httpService
+        .delete('/coupons/' + coupon.id)
+        .subscribe(() => this.couponRef.reload());
+    }
+  }
+
+  handleEdit(coupon: CouponResponse) {
+    this.bottomSheet
+      .open(CouponSheet, {
+        data: JSON.parse(JSON.stringify(coupon)),
+        hasBackdrop: true,
+        disableClose: true,
+      })
+      .afterDismissed()
+      .subscribe((result: CouponRequest) => {
+        if (!result) {
+          return;
+        }
+
+        const data = {
+          ...coupon,
+          ...result,
+        };
+
+        this.httpService
+          .update<CouponResponse>(`/coupons/${coupon.id}`, data)
+          .subscribe(() => this.couponRef.reload());
+      });
+  }
+
+  openCouptonSheet(): void {
+    this.bottomSheet
+      .open(CouponSheet, { data: {}, hasBackdrop: true, disableClose: true })
+      .afterDismissed()
+      .subscribe((result: CouponRequest) => {
+        if (!result) {
+          return;
+        }
+
+        this.httpService
+          .create<CouponRequest>('/coupons', result)
+          .subscribe(() => {
+            this.couponRef.reload();
+          });
+      });
+  }
+}
--- src/app/domains/shopping/shopping.ts
+++ src/app/domains/shopping/shopping.ts
@@ -1,72 +1,13 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
-import { DatePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import {
-  MatBottomSheet,
-  MatBottomSheetModule,
-} from '@angular/material/bottom-sheet';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-
-import { HttpService } from '../../core/services/http-service';
-import { IsExpiredPipe } from '../dashboard/pipes/table-pipes';
-import { CouponRequest, CouponResponse } from './models';
-import { CouponSheet } from './sheets/coupon-sheet';
+import { CouponsComponent } from './coupons/coupons';
 
 @Component({
   selector: 'ym-shopping',
   template: `
     <div class="container">
       <div class="left">
-        <div class="title">
-          <span>Coupons</span>
-        </div>
-
-        <div class="list">
-          @if (couponRef.isLoading()) {
-          <h2>Loading...</h2>
-          } @else if (couponRef.error()) {
-          <h2>ERROR!!!!</h2>
-          } @else {
-          <!-- -->
-          <mat-list>
-            @for (coupon of couponRef.value(); track coupon.id) {
-            <mat-list-item [class.expired]="coupon.expiredAt | isExpired">
-              <mat-icon matListItemIcon svgIcon="coupon">coupon</mat-icon>
-              <div matListItemMeta>
-                <mat-icon (click)="handleEdit(coupon)">edit</mat-icon>
-                <mat-icon (click)="handleDelete(coupon)">delete</mat-icon>
-              </div>
-              <div matListItemTitle>{{ coupon.name }}</div>
-              <div matListItemLine>{{ coupon.expiredAt | date }}</div>
-              @if(coupon.website) {
-              <div matListItemLine>
-                <a [href]="coupon.website" target="_blank" rel="noreferrer">{{
-                  coupon.website
-                }}</a>
-              </div>
-              }
-            </mat-list-item>
-            @if (!$last) {
-            <mat-divider></mat-divider>
-            }
-            <!-- -->
-            } @empty {
-            <h2>No data...</h2>
-            }
-            <!-- -->
-          </mat-list>
-          }
-        </div>
-
-        <div class="actions">
-          <button matFab (click)="openCouptonSheet()">
-            <mat-icon>add</mat-icon>
-          </button>
-        </div>
+        <ym-shopping-coupons />
       </div>
 
       <div class="right">
@@ -84,75 +25,6 @@ import { CouponSheet } from './sheets/coupon-sheet';
   `,
   styleUrl: './shopping.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    DatePipe,
-    IsExpiredPipe,
-    MatBottomSheetModule,
-    MatButtonModule,
-    MatDividerModule,
-    MatIconModule,
-    MatListModule,
-  ],
+  imports: [CouponsComponent],
 })
-export class ShoppingComponent {
-  private readonly bottomSheet = inject(MatBottomSheet);
-  private readonly httpService = inject(HttpService);
-
-  protected couponRef = rxResource({
-    stream: () => this.httpService.get<CouponResponse[]>('/coupons'),
-    defaultValue: [],
-  });
-
-  handleDelete(coupon: CouponResponse) {
-    const confirmed = confirm(
-      'Are you sure to delete the coupon: ' + coupon.name + '?'
-    );
-
-    if (confirmed) {
-      this.httpService
-        .delete('/coupons/' + coupon.id)
-        .subscribe(() => this.couponRef.reload());
-    }
-  }
-
-  handleEdit(coupon: CouponResponse) {
-    this.bottomSheet
-      .open(CouponSheet, {
-        data: JSON.parse(JSON.stringify(coupon)),
-        hasBackdrop: true,
-        disableClose: true,
-      })
-      .afterDismissed()
-      .subscribe((result: CouponRequest) => {
-        if (!result) {
-          return;
-        }
-
-        const data = {
-          ...coupon,
-          ...result,
-        };
-
-        this.httpService
-          .update<CouponResponse>(`/coupons/${coupon.id}`, data)
-          .subscribe(() => this.couponRef.reload());
-      });
-  }
-
-  openCouptonSheet(): void {
-    this.bottomSheet
-      .open(CouponSheet, { data: {}, hasBackdrop: true, disableClose: true })
-      .afterDismissed()
-      .subscribe((result: CouponRequest) => {
-        if (!result) {
-          return;
-        }
-
-        this.httpService
-          .create<CouponRequest>('/coupons', result)
-          .subscribe(() => {
-            this.couponRef.reload();
-          });
-      });
-  }
-}
+export class ShoppingComponent {}
