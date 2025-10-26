0a233490b2709f8dd883c7a0b25628077528fe99
Sebastian Kloppe <yeahdev@gmail.com>
2025-10-26 22:28:25 +0100

    use browser lang for translation language

--- src/app/core/configs/transloco-loader.ts
+++ src/app/core/configs/transloco-loader.ts
@@ -1,5 +1,9 @@
 import { inject, Injectable } from '@angular/core';
-import { Translation, TranslocoLoader } from '@jsverse/transloco';
+import {
+  getBrowserLang,
+  Translation,
+  TranslocoLoader,
+} from '@jsverse/transloco';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
@@ -7,6 +11,7 @@ export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+    const bl = getBrowserLang();
+    return this.http.get<Translation>(`/assets/i18n/${bl}.json`);
   }
 }
